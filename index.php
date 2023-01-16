6f15fdc61f07d89082afcf13e787cb9fe2709006
Sofia-Citrini <sofiacitrini@gmail.com>
2023-01-16 15:45:39 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>php-oop-2</title>
+
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+    <header>
+        <div class="container py-3">
+            <h1 class="text-center">PET SHOP</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+
+        </div>
+    </main> 
+</body>
+</html>
